b4e23a04e0b30b8870a42d5f2758216e3329f72e
Jakub Pawlowicz <contact@jakubpawlowicz.com>
2011-04-03 16:42:20 +0200

    Added support for --pregzip option for outputting gzipped version of enhanced CSS.
    * Added binary tests.

---
+++ test/binary-test.js
@@ -0,0 +1,73 @@
+var vows = require('vows'),
+  assert = require('assert'),
+  fs = require('fs'),
+  path = require('path'),
+  exec = require('child_process').exec;
+  
+var checkFiles = function(fileName, options) {
+  var pathToFile = function(noEmbed, pregzip) {
+    return '/tmp/' + fileName + (noEmbed ? '-noembed' : '') + '.css' + (pregzip ? '.gz' : '');
+  };
+  
+  assert.equal(path.existsSync(pathToFile()), true);
+  assert.equal(path.existsSync(pathToFile(true)), !!options.noEmbed);
+  assert.equal(path.existsSync(pathToFile(false, true)), !!options.pregzip);
+  assert.equal(path.existsSync(pathToFile(true, true)), !!(options.pregzip && options.noEmbed));
+};
+  
+vows.describe('enhance css binary').addBatch({
+  'no options': {
+    topic: function() {
+      exec('./bin/enhancecss -h', this.callback);
+    },
+    'should give usage info': function(error, data) {
+      assert.equal(0, data.indexOf('Usage:'));
+    }
+  },
+  'simple embed': {
+    topic: function() {
+      exec("echo 'a{background:url(/test/data/gradient.png?embed);}' | enhancecss -o /tmp/test1.css", this.callback);
+    },
+    'should give empty output': function(error, data) {
+      assert.isEmpty(data);
+    },
+    'should create valid files': function() {
+      checkFiles('test1', { noEmbed: false, pregzip: false });
+    },
+    teardown: function() {
+      fs.unlink('/tmp/test1.css');
+    }
+  },
+  'embed with --noembedversion option': {
+    topic: function() {
+      exec("echo 'a{background:url(/test/data/gradient.png?embed);}' | enhancecss --noembedversion -o /tmp/test2.css", this.callback);
+    },
+    'should give empty output': function(error, data) {
+      assert.isEmpty(data);
+    },
+    'should create valid files': function() {
+      checkFiles('test2', { noEmbed: true, pregzip: false });
+    },
+    teardown: function() {
+      fs.unlink('/tmp/test2.css');
+      fs.unlink('/tmp/test2-noembed.css');
+    }
+  },
+  'noembed & pregzipped version options': {
+    topic: function() {
+      exec("echo 'a{background:url(/test/data/gradient.png?embed);}' | enhancecss --noembedversion --pregzip -o /tmp/test3.css", this.callback);
+    },
+    'should give empty output': function(error, data) {
+      assert.isEmpty(data);
+    },
+    'should create valid files': function() {
+      checkFiles('test3', { noEmbed: true, pregzip: true });
+    },
+    teardown: function() {
+      fs.unlink('/tmp/test3.css');
+      fs.unlink('/tmp/test3.css.gz');
+      fs.unlink('/tmp/test3-noembed.css');
+      fs.unlink('/tmp/test3-noembed.css.gz');
+    }
+  }
+}).export(module);
--- test/embed-test.js
+++ test/embed-test.js
@@ -1,12 +1,15 @@
 var vows = require('vows'),
   assert = require('assert'),
   fs = require('fs'),
+  exec = require('child_process').exec,
   EnhanceCSS = require('../lib/enhance.js');
   
 var runOn = function(css, extraOptions) {
   if (!extraOptions) extraOptions = {};
   
-  return new EnhanceCSS(append({ rootPath: process.cwd() }, extraOptions)).process(css);
+  return function() {
+    return new EnhanceCSS(append({ rootPath: process.cwd() }, extraOptions)).process(css, this.callback);
+  }
 };
 
 var append = function(o1, o2) {
@@ -23,47 +26,54 @@ var mtime = function(imageName) {
 };
 
 vows.describe('embedding images').addBatch({
-  'no embed': {
-    topic: 'a{background:url(/test/data/gradient.jpg);}',
-    'for no ?embed parameter': function(css) {
-      assert.equal(runOn(css).embedded, 'a{background:url(/test/data/gradient.jpg?' + mtime('gradient.jpg') + ');}');
+  'plain content': {
+    topic: runOn('div{width:100px;height:50px}'),
+    'should be left intact': function(data) {
+      assert.equal(data.embedded.plain, data.original);
     }
   },
-  'urls with special characters': {
-    topic: function() {
-      return function(chr) { return "a{background:url(" + chr + "/test/data/gradient.jpg" + chr + ");}"; }
-    },
-    'should be processed #1': function(css) {
-      assert.equal(runOn(css('"')).embedded, 'a{background:url(/test/data/gradient.jpg?' + mtime('gradient.jpg') + ');}')
-    },
-    'should be processed #2': function(css) {
-      assert.equal(runOn(css('\'')).embedded, 'a{background:url(/test/data/gradient.jpg?' + mtime('gradient.jpg') + ');}')
+  'no embed': {
+    topic: runOn('a{background:url(/test/data/gradient.jpg);}'),
+    'for no ?embed parameter': function(data) {
+      assert.equal(data.embedded.plain, 'a{background:url(/test/data/gradient.jpg?' + mtime('gradient.jpg') + ');}');
+    }
+  },
+  'urls with special characters #1': {
+    topic: runOn("a{background:url(\"/test/data/gradient.jpg\");}"),
+    'should be processed': function(data) {
+      assert.equal(data.embedded.plain, 'a{background:url(/test/data/gradient.jpg?' + mtime('gradient.jpg') + ');}')
+    }
+  },
+  'urls with special characters #2': {
+    topic: runOn("a{background:url('/test/data/gradient.jpg');}"),
+    'should be processed': function(data) {
+      assert.equal(data.embedded.plain, 'a{background:url(/test/data/gradient.jpg?' + mtime('gradient.jpg') + ');}')
     }
   },
   'already embedded': {
-    topic: 'a{background:url(data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQH/2wBDAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQH/wAARCAAKAAoDASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwD0D/h7BrH/AEG7P/vmH/Gj/h7BrH/Qbs/++Yf8a/kX/tbVf+gnqH/gbc//AByj+1tV/wCgnqH/AIG3P/xyv9DFmeH0/wCEnL+n/LqP9z+75/gux/qgsJlWn/CRl/T/AJcR/wCnfkf/2Q==)}',
-    'should not be changed': function(css) {
-      assert.equal(runOn(css).embedded, css)
+    topic: runOn('a{background:url(data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQH/2wBDAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQEBAQH/wAARCAAKAAoDASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwD0D/h7BrH/AEG7P/vmH/Gj/h7BrH/Qbs/++Yf8a/kX/tbVf+gnqH/gbc//AByj+1tV/wCgnqH/AIG3P/xyv9DFmeH0/wCEnL+n/LqP9z+75/gux/qgsJlWn/CRl/T/AJcR/wCnfkf/2Q==)}'),
+    'should not be changed': function(data) {
+      assert.equal(data.embedded.plain, data.original)
     }
   },
   'same urls with mixed characters': {
-    topic: "a{background:url('/test/data/gradient.jpg?embed');} div{background:url(/test/data/gradient.jpg?embed);}",
-    'should not be embedded': function(css) {
-      assert.equal(runOn(css).embedded, 
+    topic: runOn("a{background:url('/test/data/gradient.jpg?embed');} div{background:url(/test/data/gradient.jpg?embed);}"),
+    'should not be embedded': function(data) {
+      assert.equal(data.embedded.plain, 
         'a{background:url(/test/data/gradient.jpg?' + mtime('gradient.jpg') + ');} ' +
         'div{background:url(/test/data/gradient.jpg?' + mtime('gradient.jpg') + ');}')
     }
   },
   'url with relative parts': {
-    topic: 'a{background:url(/test/data/../data/gradient.png)}',
-    'should be normalized': function(css) {
-      assert.equal(runOn(css).embedded, 'a{background:url(/test/data/gradient.png?' + mtime('gradient.png') + ')}');
+    topic: runOn('a{background:url(/test/data/../data/gradient.png)}'),
+    'should be normalized': function(data) {
+      assert.equal(data.embedded.plain, 'a{background:url(/test/data/gradient.png?' + mtime('gradient.png') + ')}');
     }
   },
   'external url': {
-    topic: 'a{background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAIAAAACUFjqAAAAL0lEQVQYlWP8vPYPA27A8vPDT3zSH+9/xC/9CZ/0+3dv8Em/+4hf+tNrSqQpMRwASS8b5/ih3SAAAAAASUVORK5CYII=)}',
-    'should not be transformed': function(css) {
-      assert.equal(runOn(css).embedded, css);
+    topic: runOn('a{background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAIAAAACUFjqAAAAL0lEQVQYlWP8vPYPA27A8vPDT3zSH+9/xC/9CZ/0+3dv8Em/+4hf+tNrSqQpMRwASS8b5/ih3SAAAAAASUVORK5CYII=)}'),
+    'should not be transformed': function(data) {
+      assert.equal(data.embedded.plain, data.original);
     }
   },
   'one file to be embedded': {
@@ -71,59 +81,59 @@ vows.describe('embedding images').addBatch({
       return function(type) { return 'a{background:url(/test/data/gradient.' + type + '?embed)}'; };
     },
     'should give Base64 embedded jpg': function(css) {
-      assert.equal(runOn(css('jpg')).embedded, "a{background:url(data:image/jpeg;base64," + base64('gradient.jpg') + ")}")
+      assert.equal(runOn(css('jpg'))().embedded.plain, "a{background:url(data:image/jpeg;base64," + base64('gradient.jpg') + ")}")
     },
     'should give Base64 embedded png': function(css) {
-      assert.equal(runOn(css('png')).embedded, "a{background:url(data:image/png;base64," + base64('gradient.png') + ")}")
+      assert.equal(runOn(css('png'))().embedded.plain, "a{background:url(data:image/png;base64," + base64('gradient.png') + ")}")
     },
     'should give Base64 embedded gif': function(css) {
-      assert.equal(runOn(css('gif')).embedded, "a{background:url(data:image/gif;base64," + base64('gradient.gif') + ")}")
+      assert.equal(runOn(css('gif'))().embedded.plain, "a{background:url(data:image/gif;base64," + base64('gradient.gif') + ")}")
     },
     'should give Base64 embedded svg': function(css) {
-      assert.equal(runOn(css('svg')).embedded, "a{background:url(data:image/svg+xml;base64," + base64('gradient.svg') + ")}")
+      assert.equal(runOn(css('svg'))().embedded.plain, "a{background:url(data:image/svg+xml;base64," + base64('gradient.svg') + ")}")
     }
   },
   'more than one file marked with ?embed': {
-    topic: 'a{background:url(/test/data/gradient.jpg?embed)} div{background:url(/test/data/gradient.jpg?embed)}',
-    'should not embed to Base64': function(css) {
-      assert.equal(runOn(css).embedded,
+    topic: runOn('a{background:url(/test/data/gradient.jpg?embed)} div{background:url(/test/data/gradient.jpg?embed)}'),
+    'should not embed to Base64': function(data) {
+      assert.equal(data.embedded.plain,
         'a{background:url(/test/data/gradient.jpg?' + mtime('gradient.jpg') + ')} div{background:url(/test/data/gradient.jpg?' + mtime('gradient.jpg') + ')}'
       )
     }
   },
   'more than one file and only one marked with ?embed': {
-    topic: 'a{background:url(/test/data/gradient.png)} div{background:url(/test/data/gradient.png?embed)} p{border-image:url(/test/data/gradient.png)}',
-    'should embed one file to Base64': function(css) {
-      assert.equal(runOn(css).embedded,
+    topic: runOn('a{background:url(/test/data/gradient.png)} div{background:url(/test/data/gradient.png?embed)} p{border-image:url(/test/data/gradient.png)}'),
+    'should embed one file to Base64': function(data) {
+      assert.equal(data.embedded.plain,
         ['a{background:url(/test/data/gradient.png?' + mtime('gradient.png') + ')}',
          'div{background:url(data:image/png;base64,' + base64('gradient.png') + ')}',
          'p{border-image:url(/test/data/gradient.png?' + mtime('gradient.png') + ')}'].join(' '))
     }
   },
   'not embedded files': {
-    topic: 'a{background:url(/test/data/gradient.png)} div{background:url(/test/data/gradient.jpg)}',
-    'should get mtime timestamp': function(css) {
-      assert.equal(runOn(css).embedded,
+    topic: runOn('a{background:url(/test/data/gradient.png)} div{background:url(/test/data/gradient.jpg)}'),
+    'should get mtime timestamp': function(data) {
+      assert.equal(data.embedded.plain,
         'a{background:url(/test/data/gradient.png?' + mtime('gradient.png') + ')} div{background:url(/test/data/gradient.jpg?' + mtime('gradient.jpg') + ')}')
     }
   },
   'not found files': {
-    topic: 'a{background:url(/test/data/gradient2.png)}',
-    'should be left intact': function(css) {
-      assert.equal(runOn(css).embedded, css);
+    topic: runOn('a{background:url(/test/data/gradient2.png)}'),
+    'should be left intact': function(data) {
+      assert.equal(data.embedded.plain, data.original);
     }
   },
   'adding assets hosts': {
     topic: 'a{background:url(/test/data/gradient.png)} p{background:url(/test/data/gradient.jpg)} div{background:url(/test/data/gradient.gif)}',
     'single': function(css) {
-      assert.equal(runOn(css, { assetHosts: 'assets.example.com' }).embedded,
+      assert.equal(runOn(css, { assetHosts: 'assets.example.com' })().embedded.plain,
         ['a{background:url(//assets.example.com/test/data/gradient.png?' + mtime('gradient.png') + ')}',
          'p{background:url(//assets.example.com/test/data/gradient.jpg?' + mtime('gradient.jpg') + ')}',
          'div{background:url(//assets.example.com/test/data/gradient.gif?' + mtime('gradient.gif') + ')}'].join(' ')
       )
     },
     'multiple': function(css) {
-      assert.equal(runOn(css, { assetHosts: 'assets[0,1,2].example.com' }).embedded,
+      assert.equal(runOn(css, { assetHosts: 'assets[0,1,2].example.com' })().embedded.plain,
         ['a{background:url(//assets0.example.com/test/data/gradient.png?' + mtime('gradient.png') + ')}',
          'p{background:url(//assets1.example.com/test/data/gradient.jpg?' + mtime('gradient.jpg') + ')}',
          'div{background:url(//assets2.example.com/test/data/gradient.gif?' + mtime('gradient.gif') + ')}'].join(' ')
@@ -134,30 +144,85 @@ vows.describe('embedding images').addBatch({
   'getting non-embedded version (IE7)': {
     topic: 'a{background:url(/test/data/gradient.png)} p{background:url(/test/data/gradient.jpg)}',
     'not by default': function(css) {
-      assert.isUndefined(runOn(css).notEmbedded)
+      assert.isUndefined(runOn(css)().notEmbedded)
     },
     'if requested': function(css) {
-      assert.equal(runOn(css, { noEmbedVersion: true }).notEmbedded,
+      assert.equal(runOn(css, { noEmbedVersion: true })().notEmbedded.plain,
         'a{background:url(/test/data/gradient.png?' + mtime('gradient.png') + ')} p{background:url(/test/data/gradient.jpg?' + mtime('gradient.jpg') + ')}')
     }
   },
   'getting non-embedded version (IE7) with embed': {
     topic: 'a{background:url(/test/data/gradient.png?embed)}',
     'if requested': function(css) {
-      assert.equal(runOn(css, { noEmbedVersion: true }).notEmbedded,
+      assert.equal(runOn(css, { noEmbedVersion: true })().notEmbedded.plain,
         'a{background:url(/test/data/gradient.png?' + mtime('gradient.png') + ')}')
     }
   },
   'getting non-embedded version (IE7) with duplicates and embed': {
     topic: 'a{background:url(/test/data/gradient.png?embed)} p{background:url(/test/data/gradient.png?embed)}',
     'if requested': function(css) {
-      assert.equal(runOn(css, { noEmbedVersion: true }).notEmbedded,
+      assert.equal(runOn(css, { noEmbedVersion: true })().notEmbedded.plain,
         'a{background:url(/test/data/gradient.png?' + mtime('gradient.png') + ')} p{background:url(/test/data/gradient.png?' + mtime('gradient.png') + ')}')
     }
   }
 }).addBatch({
+  'compressed content': {
+    topic: runOn('a{background:#fff}'),
+    'not by default': function(data) {
+      assert.isUndefined(data.embedded.compressed);
+    }
+  },
+  'compressed embedded content': {
+    topic: runOn('a{background:#fff}', { pregzip: true }),
+    'should be string': function(data) {
+      assert.ok(Buffer.isBuffer(data.embedded.compressed));
+    },
+    'should be different from uncompressed': function(data) {
+      assert.notEqual(data.embedded.compressed.toString(), data.embedded.plain);
+    },
+    'should be different from original': function(data) {
+      assert.notEqual(data.embedded.compressed.toString(), data.original);
+    },
+    'uncompressing': {
+      topic: function(data) {
+        fs.writeFileSync('/tmp/data1.gz', data.embedded.compressed);
+        exec("gzip -c -d /tmp/data1.gz", this.callback);
+      },
+      'should be equal to embedded': function(error, uncompressed) {
+        assert.equal('a{background:#fff}', uncompressed);
+      },
+      teardown: function() {
+        fs.unlinkSync('/tmp/data1.gz');
+      }
+    }
+  },
+  'compressed non-embedded content': {
+    topic: runOn('a{background:#fff}', { pregzip: true, noEmbedVersion: true}),
+    'should be string': function(data) {
+      assert.ok(Buffer.isBuffer(data.notEmbedded.compressed));
+    },
+    'should be different from uncompressed': function(data) {
+      assert.notEqual(data.notEmbedded.compressed.toString(), data.notEmbedded.plain);
+    },
+    'should be different from original': function(data) {
+      assert.notEqual(data.notEmbedded.compressed.toString(), data.original);
+    },
+    'uncompressing': {
+      topic: function(data) {
+        fs.writeFileSync('/tmp/data2.gz', data.notEmbedded.compressed);
+        exec("gzip -c -d /tmp/data2.gz", this.callback);
+      },
+      'should be equal to embedded': function(error, uncompressed) {
+        assert.equal('a{background:#fff}', uncompressed);
+      },
+      teardown: function() {
+        fs.unlinkSync('/tmp/data2.gz');
+      }
+    }
+  }
+}).addBatch({
   'list of missing files': {
-    topic: runOn('a{background:url(/test/data/gradient2.png)} p{background:url(/test/data/gradient2.jpg)}').missing,
+    topic: runOn('a{background:url(/test/data/gradient2.png)} p{background:url(/test/data/gradient2.jpg)}')().missing,
     'should have both files': function(missing) {
       assert.length(missing, 2);
     },
@@ -167,7 +232,7 @@ vows.describe('embedding images').addBatch({
     }
   },
   'list of not embedded files (duplicates)': {
-    topic: runOn('a{background:url(/test/data/gradient.png?embed)} p{background:url(/test/data/gradient.png?embed)}').duplicates,
+    topic: runOn('a{background:url(/test/data/gradient.png?embed)} p{background:url(/test/data/gradient.png?embed)}')().duplicates,
     'should have one file': function(duplicates) {
       assert.length(duplicates, 1);
     },
